06f053f111b5ff61e4a833b226c64e76a34b0dae
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-09-13 20:45:43 +0100

    Optimised runtime of 409E from 422ms to 142ms

--- 409E.js
+++ 409E.js
@@ -1,21 +1,24 @@
 /**
  * @param {string} s
  * @return {number}
- * Runtime: 427 ms, faster than 5.12% of JavaScript online submissions for Longest Palindrome.
-Memory Usage: 48.4 MB, less than 5.46% of JavaScript online submissions for Longest Palindrome.
+ Runtime: 142 ms, faster than 5.74% of JavaScript online submissions for Longest Palindrome.
+Memory Usage: 46.1 MB, less than 5.40% of JavaScript online submissions for Longest Palindrome.
  */
  var longestPalindrome = function(s) {
     let lengthPal = 0;
     for (i=0; i<s.length; i++){
-        var re = new RegExp(s[i], 'g');
-        let result = s.match(re);
-        if (result.length % 2 !== 0){
-            s = s.replace(s[i], "1")
-        }
-        else if (result.length > 1){
-            lengthPal ++
-        }
-
+        if (s[i] !== "1"){
+            var re = new RegExp(s[i], 'g');
+            let result = s.match(re);
+            if (result.length % 2 !== 0){
+                s = s.replace(s[i], "1")
+            }
+            else if (result.length > 1){
+                lengthPal += result.length
+                var rep = new RegExp(s[i], 'g');
+                s = s.replace(rep, "1")
+            }
+        }   
     }
     if (lengthPal < s.length){
         lengthPal++
@@ -23,4 +26,4 @@ Memory Usage: 48.4 MB, less than 5.46% of JavaScript online submissions for Long
     return lengthPal
 };
 
-longestPalindrome("dakfnaaald")
+longestPalindrome("dakfnaaaald")
