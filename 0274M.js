107b961f8f43227b000c1724a651f45c150d58b7
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-09-29 15:14:23 +0100

    Added stats

--- 0274M.js
+++ 0274M.js
@@ -1,12 +1,26 @@
 /**
  * @param {number[]} citations
  * @return {number}
+ * Runtime: 100 ms, faster than 45.08% of JavaScript online submissions for H-Index.
+Memory Usage: 42.3 MB, less than 40.98% of JavaScript online submissions for H-Index.
  */
  var hIndex = function(citations) {
 
     sortedArray = citations.sort(function(a, b){return a - b})
 
-    console.log(sortedArray)
+    let iterations = citations.length
+    let hIndexVal = iterations
+
+    for (i=0; i<iterations; i++){
+        if (sortedArray[i] >= hIndexVal){
+            return hIndexVal
+        }
+        else{
+            hIndexVal--
+        }
+    }
+
+    return hIndexVal
     
 };
 
