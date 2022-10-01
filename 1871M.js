0d47fb52aace8851e044cf107d3c424e1cd96897
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-10-01 17:23:55 +0100

    Deque solution

--- 1871M.js
+++ 1871M.js
@@ -4,66 +4,28 @@
  * @param {number} maxJump
  * @return {boolean}
  */
-
  var canReach = function(s, minJump, maxJump) {
-
-    let iterations = s.length-1;
-    let canGetTo = {["0"]:true, [iterations]:false}
-    let sArray = s.split("")
-    let highestChange = 0
+    const n = s.length;
+    let reachables = [0]; 
     
-        if (sArray[0] === "1"){
-        return false
-        }
-    
+    for (let i = 1; i < n; ++i) {
+        const bit = s[i];
 
-    for (i=0; i<iterations; i++){
+        if (bit === "1") continue; 
 
-        if (highestChange + maxJump < iterations){
-            highestChange = i + sArray.slice(i+minJump,i+maxJump+1).lastIndexOf("0")
-            console.log(sArray.slice(i+minJump,i+maxJump+1))
-            console.log(highestChange + " highestChange and i = " + i)
-            if (sArray[highestChange+1-minJump] === "0"){
-                console.log("i changed")
-                canGetTo[highestChange+1] = true
-                canGetTo[highestChange+1-minJump] = true
-                i = highestChange - minJump - 1
-            }
-        
+        while (reachables.length > 0 && i - reachables[0] > maxJump) { 
+            reachables.shift();
+        }
 
-            else if (canGetTo[i] === true){
-                for (j=minJump; j<=maxJump; j++){
-                    if (sArray[i+j] === "0"){
-                        canGetTo[i+j] = true
-                        highestChange = i+j
-                    }
+        if (reachables.length === 0) return false; 
 
-                }        
-            }
-        }
-        else if (iterations - i >= minJump && iterations - i <= maxJump){
-            console.log(canGetTo)
-            console.log("True returned")
-            return true
-        }
-        else if (canGetTo[i] === true){
-            for (j=minJump; j<=maxJump; j++){
-                if (sArray[i+j] === "0"){
-                    canGetTo[i+j] = true
-                    highestChange = i+j
-                }
-
-            }        
-        }
-        if (canGetTo[iterations] === true){
-            console.log(canGetTo)
-            console.log("True returned")
-            return true
-        }
+        if (i - reachables[0] < minJump) continue; 
+		reachables.push(i);
     }
-    console.log(canGetTo)
-    console.log(canGetTo[iterations])
-    return canGetTo[iterations]    
+   
+    const reached = reachables.pop();
+    
+    return reached === n - 1; 
 };
 
 canReach("0101001101010101000000000000000000000000000000000000000000000000000000000000000",1,10)
