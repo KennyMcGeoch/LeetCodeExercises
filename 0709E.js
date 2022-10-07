532791cc29a8c900a95bf8c45043ea15209b1c8a
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-10-07 11:16:03 +0100

    0709E

---
+++ 0709E.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ * Runtime: 86 ms, faster than 66.12% of JavaScript online submissions for To Lower Case.
+ * Memory Usage: 41.9 MB, less than 73.55% of JavaScript online submissions for To Lower Case.
+ */
+ var toLowerCase = function(s) {
+    return s.toLocaleLowerCase()
+    
+};
