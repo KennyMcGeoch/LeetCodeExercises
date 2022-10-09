b755538f0b10f0929099b48266a98819b306c5db
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-10-09 14:35:36 +0100

    Optimised algorithm to use sets instead of a loop

--- 0217E.js
+++ 0217E.js
@@ -1,20 +1,31 @@
 /**
  * @param {number[]} nums
  * @return {boolean}
- * Runtime: 331 ms, faster than 16.96% of JavaScript online submissions for Contains Duplicate.
-Memory Usage: 50.8 MB, less than 63.00% of JavaScript online submissions for Contains Duplicate.
+ * Runtime: 154 ms, faster than 43.04% of JavaScript online submissions for Contains Duplicate.
+ * Memory Usage: 50.8 MB, less than 62.28% of JavaScript online submissions for Contains Duplicate.
  */
+
  var containsDuplicate = function(nums) {
 
-    nums.sort()
-    let iterations = nums.length - 1
+    let numsSet = new Set(nums).size    
+    if (nums.length === numsSet){return false}
 
-    for (i=0; i<iterations; i++){
-        if(nums[i] === nums[i+1]){
-            return true
-        }
-    }
-
-    return false
+    return true
     
 };
+
+//  Slower solution using sort and iteration
+//  var containsDuplicate = function(nums) {
+
+//     nums.sort()
+//     let iterations = nums.length - 1
+
+//     for (i=0; i<iterations; i++){
+//         if(nums[i] === nums[i+1]){
+//             return true
+//         }
+//     }
+
+//     return false
+    
+// };
