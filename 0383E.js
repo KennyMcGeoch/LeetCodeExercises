04a95f80af97a43c71b2624e82cf862897ea7009
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-10-10 12:59:35 +0100

    Changed implementation to hashmap to improve performance

--- 0383E.js
+++ 0383E.js
@@ -2,14 +2,27 @@
  * @param {string} ransomNote
  * @param {string} magazine
  * @return {boolean}
+ * Runtime: 158 ms, faster than 30.98% of JavaScript online submissions for Ransom Note.
+ * Memory Usage: 45 MB, less than 58.53% of JavaScript online submissions for Ransom Note.
  */
  var canConstruct = function(ransomNote, magazine) {
+    let magazineContents = {}
+
+    for (i=0; i<magazine.length; i++){
+        if (magazineContents[magazine[i]] === undefined){
+            magazineContents[magazine[i]] = 1
+        }
+        else{
+            magazineContents[magazine[i]]++
+        }
+    }
+
     for (i=0; i<ransomNote.length; i++){
-        let test = ransomNote.charAt(i);
-        var check = new RegExp (test, "g");
-        count = ransomNote.match(check).length - (magazine.match(check)?.length ?? 0)
-        if (count > 0){
+        if (magazineContents[ransomNote[i]] === undefined || magazineContents[ransomNote[i]] === 0){
             return false
+        }
+        else{
+            magazineContents[ransomNote[i]]--
         }
     }
     return true
