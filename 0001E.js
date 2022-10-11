096ce428f532c011727bba00662588bf92d899fa
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-10-11 15:24:52 +0100

    Used a hashmap instead of a nested loop. Runtime halved

--- 0001E.js
+++ 0001E.js
@@ -2,19 +2,19 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- * Runtime: 244 ms, faster than 14.47% of JavaScript online submissions for Two Sum.
-Memory Usage: 42.2 MB, less than 81.30% of JavaScript online submissions for Two Sum.
+ * Runtime: 136 ms, faster than 47.93% of JavaScript online submissions for Two Sum.
+ * Memory Usage: 42.7 MB, less than 51.81% of JavaScript online submissions for Two Sum.
  */
- var twoSum = function(nums, target) {
+var twoSum = function(nums, target) {
+
+    let resultStore = {}
+
     for (i=0; i<nums.length; i++){
-        let targetNo = target - nums[i]
-        for (j=i+1; j<nums.length; j++){
-            if (nums[j] === targetNo){
-                return [i, j]
-            }
+        if (resultStore[nums[i]] !== undefined){
+            return [resultStore[nums[i]],i]
         }
-    }
-    
+        resultStore[target - nums[i]] = i
+    }    
 };
 
 twoSum([2,7,11,15],9)
