d35924ff51f6c9e36d9f6b3d211939f9dc6bb7ee
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-10-25 12:00:04 +0100

    1662E

---
+++ 1662E.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ * Runtime: 95 ms, faster than 49.74% of JavaScript online submissions for Check If Two String Arrays are Equivalent.
+ * Memory Usage: 42.2 MB, less than 40.18% of JavaScript online submissions for Check If Two String Arrays are Equivalent.
+ */
+ var arrayStringsAreEqual = function(word1, word2) {
+
+    let compOne = ""
+    let compTwo = ""
+
+    compOne = convertArrToStr(word1)
+    compTwo = convertArrToStr(word2)
+
+
+    function convertArrToStr(arr){
+        let ans = ""
+        for (i=0; i<arr.length; i++){
+            ans += arr[i]
+        }
+        return ans
+    }
+    return compOne === compTwo
+};
