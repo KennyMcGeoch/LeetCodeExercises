ae7b03784987be6a8a2a5fd8b8e64520cd77de44
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-10-29 12:07:55 +0100

    Removed coin sort as it isn't required as I iterate through whole array anyway

--- 0322M.js
+++ 0322M.js
@@ -2,15 +2,14 @@
  * @param {number[]} coins
  * @param {number} amount
  * @return {number}
- * Runtime: 425 ms, faster than 17.15% of JavaScript online submissions for Coin Change.
- * Memory Usage: 51.1 MB, less than 14.68% of JavaScript online submissions for Coin Change.
+ * Runtime: 262 ms, faster than 34.01% of JavaScript online submissions for Coin Change.
+ * Memory Usage: 50.8 MB, less than 15.71% of JavaScript online submissions for Coin Change.
  */
  var coinChange = function(coins, amount) {
     
     let minCoin = {0:0}
     let iterations = amount + 1
     let iterationsC = coins.length
-    coins.sort((a,b)=>b-a)
 
     for (i=1; i<iterations;i++){
         for (j=0; j<iterationsC; j++){
