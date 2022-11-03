b4b7c62d59efb0f0001520bf6c9d512c543e8fac
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-11-03 17:16:27 +0000

    Added stats

--- 1527E.sql
+++ 1527E.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
+# Runtime: 316 ms, faster than 86.70% of MySQL online submissions for Patients With a Condition.
+# Memory Usage: 0B, less than 100.00% of MySQL online submissions for Patients With a Condition.
 
 SELECT * FROM Patients WHERE conditions LIKE '% DIAB1%' OR conditions LIKE 'DIAB1%'
