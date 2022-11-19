72930ee54be20af35ad7f50e84d727e806d8b003
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-11-19 20:00:17 +0000

    Searched for ba instead of b that avoided second loop

--- 2124E.js
+++ 2124E.js
@@ -1,18 +1,15 @@
 /**
  * @param {string} s
  * @return {boolean}
- * Runtime: 106 ms, faster than 34.48% of JavaScript online submissions for Check if All A's Appears Before All B's.
- * Memory Usage: 41.9 MB, less than 67.71% of JavaScript online submissions for Check if All A's Appears Before All B's.
+ * Runtime: 58 ms, faster than 98.75% of JavaScript online submissions for Check if All A's Appears Before All B's.
+ * Memory Usage: 41.8 MB, less than 86.83% of JavaScript online submissions for Check if All A's Appears Before All B's.
  */
  var checkString = function(s) {
 
     let iterations = s.length
-    let first = s.indexOf("b")
+    let first = s.indexOf("ba")
 
     if (first < 0)return true
 
-    for (i=first+1; i<iterations; i++){
-        if (s[i] === "a") return false
-    }
-    return true
+    return false
 };
