6ab0e8a940197d992bf38f38ae484720d1f7c25a
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-11-28 18:40:14 +0000

    sort removed as not required

--- 2225M.js
+++ 2225M.js
@@ -1,8 +1,8 @@
 /**
  * @param {number[][]} matches
  * @return {number[][]}
- * Runtime: 752 ms, faster than 55.59% of JavaScript online submissions for Find Players With Zero or One Losses.
- * Memory Usage: 110.1 MB, less than 63.98% of JavaScript online submissions for Find Players With Zero or One Losses.
+ * Runtime: 542 ms, faster than 83.56% of JavaScript online submissions for Find Players With Zero or One Losses.
+ * Memory Usage: 117 MB, less than 48.95% of JavaScript online submissions for Find Players With Zero or One Losses.
  */
  var findWinners = function(matches) {
     
@@ -19,9 +19,6 @@
     for (x in hash){
         if (hash[x] < 2)solution[hash[x]].push(x)
     }
-    
-    solution[0].sort((a,b)=> a-b)
-    solution[1].sort((a,b)=> a-b)
     
     return solution
     
