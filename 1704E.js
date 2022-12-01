40662b124f20e8df253420076713b50fdb59743c
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-12-01 16:37:47 +0000

    Changed to a set from an array to improve performance

--- 1704E.js
+++ 1704E.js
@@ -1,21 +1,21 @@
 /**
  * @param {string} s
  * @return {boolean}
- * Runtime: 97 ms, faster than 71.48% of JavaScript online submissions for Determine if String Halves Are Alike.
- * Memory Usage: 43.6 MB, less than 41.02% of JavaScript online submissions for Determine if String Halves Are Alike.
+ * Runtime: 79 ms, faster than 84.38% of JavaScript online submissions for Determine if String Halves Are Alike.
+ * Memory Usage: 42.7 MB, less than 70.31% of JavaScript online submissions for Determine if String Halves Are Alike.
  */
  var halvesAreAlike = function(s) {
     
     let iterations = s.length
     let half = iterations / 2
     let count = 0
-    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
     
     for (i=0; i<half; i++){
-        if (vowels.includes(s[i])) count++
+        if (vowels.has(s[i])) count++
     }
     for (i=half; i<iterations; i++){
-        if (vowels.includes(s[i])) count--
+        if (vowels.has(s[i])) count--
     }
     return count === 0   
 };
