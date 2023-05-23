00fd0377300e57a06ebee38fd6b387876e175fd6
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-05-23 22:53:55 +0100

    replaced slice with pop

--- 0703E.js
+++ 0703E.js
@@ -1,12 +1,18 @@
 /**
  * @param {number} k
  * @param {number[]} nums
- * Runtime: 2482 ms, faster than 18.35% of JavaScript online submissions for Kth Largest Element in a Stream.
- * Memory Usage: 69.9 MB, less than 5.50% of JavaScript online submissions for Kth Largest Element in a Stream.
+ * Runtime: 1547 ms, faster than 22.93% of JavaScript online submissions for Kth Largest Element in a Stream.
+ * Memory Usage: 59.3 MB, less than 15.14% of JavaScript online submissions for Kth Largest Element in a Stream.
  */
 var KthLargest = function(k, nums) {
     this.arr = nums.sort((a,b)=>b-a)
     this.max = k
+    while (this.arr.length > k){
+        this.arr.pop()
+    }
+    while (this.arr.length < k){
+        this.arr.push(-100000)
+    }
     
 };
 
@@ -17,7 +23,7 @@ var KthLargest = function(k, nums) {
 KthLargest.prototype.add = function(val) {
     if (val < this.arr[this.max-1]) return this.arr[this.max-1]
     this.arr.push(val)
-    this.arr.sort((a,b)=>b-a).slice(0,this.max)
+    this.arr.sort((a,b)=>b-a).pop()
     return this.arr[this.max-1]
   
 };
