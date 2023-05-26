323f637b9a6bfd40d5749798464e778636ec8792
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-05-26 20:48:22 +0100

    Used Array.isArray method to filter [] instead of loose equality

--- 2625M.js
+++ 2625M.js
@@ -2,8 +2,8 @@
  * @param {any[]} arr
  * @param {number} depth
  * @return {any[]}
- * Runtime: 813 ms, faster than 8.35% of JavaScript online submissions for Flatten Deeply Nested Array.
- * Memory Usage: 108.1 MB, less than 7.10% of JavaScript online submissions for Flatten Deeply Nested Array.
+ * Runtime: 183 ms, faster than 60.45% of JavaScript online submissions for Flatten Deeply Nested Array.
+ * Memory Usage: 87 MB, less than 37.04% of JavaScript online submissions for Flatten Deeply Nested Array.
  */
 var flat = function (arr, n) {
     let ans = arr
@@ -11,14 +11,16 @@ var flat = function (arr, n) {
         let brackets = 0
         let temp = []
         for (let i=0; i<ans.length; i++){
-            if (ans[i] == "" && ans[i] !== 0)continue
-            else if (ans[i][0] === undefined) temp.push(ans[i])
-            else{
-                brackets++
-                for (let j=0; j<ans[i].length; j++){
-                    if (ans[i][j] !== undefined)temp.push(ans[i][j])
+            if (Array.isArray(ans[i])){
+                if (ans[i][0] === undefined) continue
+                else{
+                    brackets++
+                    for (let j=0; j<ans[i].length; j++){
+                        if (ans[i][j] !== undefined)temp.push(ans[i][j])
+                    }
                 }
             }
+            else temp.push(ans[i])
         }
         ans = temp
         n--
