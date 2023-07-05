ea8b019696ab6629620415710efba73b805b8114
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-07-05 01:36:04 +0100

    Optimisation in loop to be around 50% faster

---
+++ 0137Mv2.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Runtime: 62 ms, faster than 77.15% of JavaScript online submissions for Single Number II.
+Memory Usage: 42.7 MB, less than 65.60% of JavaScript online submissions for Single Number II.
+ */
+var singleNumber = function(nums) {
+    
+    let iterations = nums.length
+
+    nums.sort((a,b)=>a-b)
+    
+    for (i=0; i<iterations; i+=3){
+        if (nums[i] !== nums[i+1])return nums[i]
+    }
+    return 0
+    
+};
