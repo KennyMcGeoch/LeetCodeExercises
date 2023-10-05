674530fdb94028966b271dd294c21d2686982a40
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-10-05 20:13:07 +0100

    Added alternative implementation without hashmap for lower space complexity

--- 0229M.js
+++ 0229M.js
@@ -1,8 +1,8 @@
 /**
  * @param {number[]} nums
  * @return {number[]}
- * Runtime: 116 ms, faster than 38.60% of JavaScript online submissions for Majority Element II.
-Memory Usage: 43.4 MB, less than 73.38% of JavaScript online submissions for Majority Element II.
+ * Runtime: 59 ms, faster than 59.05% of JavaScript online submissions for Majority Element II.
+ * Memory Usage: 43.4 MB, less than 58.10% of JavaScript online submissions for Majority Element II.
  */
 
 var majorityElement = function(nums) {
@@ -24,6 +24,29 @@ var majorityElement = function(nums) {
         if (valueStore[nums[i]]>majorityNum){
             solution.push(nums[i]) 
             valueStore[nums[i]] = -1
+        }
+    }
+
+    return solution
+    
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * Runtime: 53 ms, faster than 84.76% of JavaScript online submissions for Majority Element II.
+ * Memory Usage: 45.2 MB, less than 21.59% of JavaScript online submissions for Majority Element II.
+ */
+var majorityElement2 = function(nums) {
+
+    let third = parseInt(nums.length/3)
+    let solution = []
+    nums.sort((a,b)=>a-b)
+
+    for (i=0; i<nums.length; i++){
+        if (nums[i] === nums[i+third] && (solution.includes(nums[i]) === false)){
+            solution.push(nums[i])
+            i+= third-1 // skip
         }
     }
 
