102a952b884fd36b697b463d7bed9ca1e8f34290
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-10-09 13:50:32 +0100

    Added full binary search implementation rather than use inbuilt functions

--- 0034M.js
+++ 0034M.js
@@ -24,3 +24,57 @@ var searchRange2 = function(nums, target) {
     
     return [nums.indexOf(target), nums.lastIndexOf(target)]    
 };
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ * Runtime: 46 ms, faster than 87.61% of JavaScript online submissions for Find First and Last Position of Element in Sorted Array.
+ * Memory Usage: 42.5 MB, less than 55.69% of JavaScript online submissions for Find First and Last Position of Element in Sorted Array.
+ */
+var searchRange3 = function(nums, target) {
+    
+    let first = true
+    let second = true
+    
+    if (nums[0] > target)return [-1,-1]
+    else if (nums[0] === target) first = false
+    let last = nums.length-1
+    if (nums[last] < target)return [-1,-1]
+    else if (nums[last] === target) second = false
+    
+    let solution = [0, last]
+    let min = 0
+    let max = last
+    let temp
+
+    if (first){
+        while (min !== max){
+            temp = Math.floor((min+max)/2)
+            if (nums[temp] >= target) max = temp
+            else {
+                min = temp
+                if (nums[min] < target)min++
+            }
+        }
+        if (nums[min] === target)solution[0] = min
+        else return [-1,-1]        
+    }
+    if (second){
+        min = solution[0]
+        max = solution[1]
+        while (min !== max){
+            temp = Math.floor((min+max)/2) + 1
+            if (nums[temp] > target) max = temp-1
+            else if (nums[max] === target) min = max
+            else {
+                min = temp
+            }
+        }
+        solution[1] = max
+        
+    }
+    
+    return solution
+};
