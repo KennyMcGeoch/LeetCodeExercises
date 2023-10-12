bb2026fed2e7defb5c9fc20b913bb5ac2688d497
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-10-12 13:20:55 +0100

    1095H

---
+++ 1095H.js
@@ -0,0 +1,80 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function MountainArray() {
+ *     @param {number} index
+ *     @return {number}
+ *     this.get = function(index) {
+ *         ...
+ *     };
+ *
+ *     @return {number}
+ *     this.length = function() {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {number} target
+ * @param {MountainArray} mountainArr
+ * @return {number}
+ * Runtime: 50 ms, faster than 61.90% of JavaScript online submissions for Find in Mountain Array.
+Memory Usage: 42.5 MB, less than 25.40% of JavaScript online submissions for Find in Mountain Array.
+ */
+var findInMountainArray = function(target, mountainArr) {
+    
+    let max = mountainArr.length() - 1
+    let min = 0
+    let mid = Math.floor((min+max)/2)
+    let minVal = mountainArr.get(0)
+    if (minVal === target) return 0
+    let answer = -1
+
+    function findPeak(mini, curr, maxi){
+        let temp = mountainArr.get(curr)
+        if (mountainArr.get(curr-1) > temp) return findPeak(mini, Math.floor((curr+mini)/2), curr-1)
+        else if (mountainArr.get(curr+1) > temp) return findPeak(curr+1, Math.floor((curr+maxi)/2), maxi)
+        else return [curr, temp]
+    }
+    
+    function binarySearch(mini,curr,maxi){
+        let temp = mountainArr.get(curr)
+        if (temp === target){
+            answer = curr
+            return
+        }
+        else if (mini >= maxi) {
+            if (mountainArr.get(mini) === target) answer = mini
+            return -1
+        }
+        else if (temp < target) return binarySearch(curr+1, Math.floor((curr+maxi)/2),maxi)
+        else if (temp > target) binarySearch(mini, Math.floor((curr+mini)/2),curr-1)
+        else return -1
+    }
+    
+    function binarySearchMin(mini,curr,maxi){
+        let temp = mountainArr.get(curr)
+        if (temp === target){
+            answer = curr
+            return
+        }
+        else if (mini >= maxi) {
+            if (mountainArr.get(mini) === target) answer = mini
+            return -1
+        }
+        else if (temp > target) return binarySearchMin(curr+1, Math.ceil((curr+maxi)/2),maxi)
+        else if (temp < target) binarySearchMin(mini, Math.floor((curr+mini)/2),curr-1)
+        else return -1
+    }
+    let peak = findPeak(min,mid,max)
+    if (peak[1] < target) return -1
+    else if (peak[1] === target) return peak[0]
+    
+    let searchOne = binarySearch(min, Math.floor((min+peak[0])/2),peak[0]-1)
+    if (answer > -1) return answer
+    binarySearchMin(peak[0]+1, Math.floor((max+peak[0])/2),max)
+    
+    return answer
+    
+};
--- README.md
+++ README.md
@@ -252,6 +252,7 @@ Full List of Completed Projects in Numerical Order:
 1051. Height Checker (Easy)
 1078. Occurrences After Bigram (Easy)
 1089. Duplicate Zeros (Easy)
+1095. Find in Mountain Array (Hard)
 1148. Article Views I (Easy)
 1154. Day of the Year (Easy)
 1160. Find Words That Can Be Formed by Characters (Easy)
