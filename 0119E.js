b0b5bd771798bd0673e563108176be3ad40b36ca
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-10-16 11:03:11 +0100

    Added mathematical approach instead of brute force algorithm. Oddly it's more efficient to calculate everything than to calculate half and mirror it

--- 0119E.js
+++ 0119E.js
@@ -24,3 +24,44 @@ var getRow = function(rowIndex) {
         return solution[rowIndex]
     
 };
+
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ * Runtime: 51 ms, faster than 58.99% of JavaScript online submissions for Pascal's Triangle II.
+ * Memory Usage: 42.2 MB, less than 34.31% of JavaScript online submissions for Pascal's Triangle II.
+ */
+var getRow2 = function(rowIndex) {
+    let solution = [1]
+    if (rowIndex === 0) return solution
+    let loops = Math.ceil(rowIndex/2) + 1
+    
+    for (let i=1; i<loops; i++){
+        solution.push(solution[i-1] * (rowIndex - i + 1)/i)
+    }
+     
+     loops = Math.floor(rowIndex/2) - 1
+     
+     for (let i=loops; i>-1; i--){
+         solution.push(solution[i])
+     }
+     
+     return solution    
+};
+
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ * Runtime: 37 ms, faster than 98.94% of JavaScript online submissions for Pascal's Triangle II.
+ * Memory Usage: 41.9 MB, less than 54.33% of JavaScript online submissions for Pascal's Triangle II.
+ */
+var getRow3 = function(rowIndex) {
+    let solution = [1]
+    if (rowIndex === 0) return solution
+    for (let i=1; i<rowIndex; i++){
+        solution.push(solution[i-1] * (rowIndex - i + 1)/i)
+    }
+     solution.push(1)
+     return solution
+    
+};
