76959eadefa8e288c1fea6699987d2325ab9aa35
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-10-23 13:33:13 +0100

    Added alternate solutions

--- 0342E.js
+++ 0342E.js
@@ -1,8 +1,8 @@
 /**
  * @param {number} n
  * @return {boolean}
- * Runtime: 112 ms, faster than 56.93% of JavaScript online submissions for Power of Four.
- * Memory Usage: 43.7 MB, less than 44.71% of JavaScript online submissions for Power of Four.
+ * Runtime: 49 ms, faster than 95.58% of JavaScript online submissions for Power of Four.
+ * Memory Usage: 43.3 MB, less than 63.19% of JavaScript online submissions for Power of Four.
  */
  var isPowerOfFour = function(n) {
     if ( n === 1) return true
@@ -13,4 +13,26 @@
         if (n[i] !== "0")return false
     }
     return true    
+};
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ * Runtime: 54 ms, faster than 83.80% of JavaScript online submissions for Power of Four.
+ * Memory Usage: 43.1 MB, less than 74.30% of JavaScript online submissions for Power of Four.
+ */
+var isPowerOfFour2 = function(n) {
+    if (n === 1) return true
+    while (n > 4) n /= 4
+    return n === 4
+};
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ * Runtime: 56 ms, faster than 74.30% of JavaScript online submissions for Power of Four.
+ * Memory Usage: 43.8 MB, less than 21.15% of JavaScript online submissions for Power of Four.
+ */
+var isPowerOfFour3 = function(n) {
+    return Number.isInteger(Math.log(n)/Math.log(4))
 };
