b1a28aaf64e340115da51eb3414392a2bc6d6cfc
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-10-30 11:10:24 +0000

    Added alternate solution

--- 1356E.js
+++ 1356E.js
@@ -24,4 +24,26 @@
     
 };
 
-sortByBits([0,1,2,3,4,5,6,7,8])
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ * Runtime: 71 ms, faster than 69.91% of JavaScript online submissions for Sort Integers by The Number of 1 Bits.
+ * Memory Usage: 44.9 MB, less than 56.48% of JavaScript online submissions for Sort Integers by The Number of 1 Bits.
+ */
+var sortByBits = function(arr) {
+
+    let iterations = arr.length
+    let ans = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    arr.sort((a,b)=>a-b)
+
+    for (i=0; i<iterations; i++){
+        let test = arr[i].toString(2)
+        let total = 0
+        for (j=0; j<test.length;j++){
+                if (test[j]==="1")total++
+        }
+        ans[total].push(arr[i])
+    }
+    return ans.flat()
+    
+};
