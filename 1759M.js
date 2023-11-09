4ef74120892fdb573723c4670c482b572c09c988
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-11-09 00:29:06 +0000

    Added more space efficient version

--- 1759M.js
+++ 1759M.js
@@ -1,8 +1,8 @@
 /**
  * @param {string} s
  * @return {number}
- * Runtime: 98 ms, faster than 88.00% of JavaScript online submissions for Count Number of Homogenous Substrings.
- * Memory Usage: 49.3 MB, less than 28.00% of JavaScript online submissions for Count Number of Homogenous Substrings.
+ * Runtime: 54 ms, faster than 97.78% of JavaScript online submissions for Count Number of Homogenous Substrings.
+ * Memory Usage: 49.1 MB, less than 6.67% of JavaScript online submissions for Count Number of Homogenous Substrings.
  */
  var countHomogenous = function(s) {
 
@@ -25,4 +25,28 @@
     
 };
 
-countHomogenous("abbcccaa")
+/**
+ * @param {string} s
+ * @return {number}
+ * Runtime: 58 ms, faster than 93.33% of JavaScript online submissions for Count Number of Homogenous Substrings.
+ * Memory Usage: 45.6 MB, less than 80.00% of JavaScript online submissions for Count Number of Homogenous Substrings.
+ */
+var countHomogenous = function(s) {
+
+    let count = 1
+    let runningCount = 1
+    let iterations = s.length - 1
+
+    for (i=0; i<iterations; i++){
+        if (s[i] === s[i+1]){
+            count++
+        }
+        else{
+            count = 1
+        }
+        runningCount += count
+    }
+
+    return runningCount % 1000000007
+    
+};
