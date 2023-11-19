4dc731676de07cc8626d629af4a5b5b52612893a
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-11-19 00:47:26 +0000

    Added version with 1 less variable

--- 1887M.js
+++ 1887M.js
@@ -15,3 +15,19 @@ var reductionOperations = function(nums) {
     return total
     
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Runtime: 180 ms, faster than 95.65% of JavaScript online submissions for Reduction Operations to Make the Array Elements Equal.
+ * Memory Usage: 54.2 MB, less than 73.91% of JavaScript online submissions for Reduction Operations to Make the Array Elements Equal.
+ */
+var reductionOperations = function(nums) {
+    nums.sort((a,b)=>a-b)
+    let total = 0 
+    for (let i=1; i<nums.length; i++){
+        if (nums[i] !== nums[i-1])total += nums.length-i
+    }
+    return total
+    
+};
