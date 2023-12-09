fd7a221b5d371f781469e23cbbe6f9d782caa757
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-12-09 17:10:08 +0000

    Optimised if logic to reduce operations

--- 0606E.js
+++ 0606E.js
@@ -41,3 +41,50 @@ var tree2str = function(root) {
         return str
     
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ * Runtime: 52 ms, faster than 96.64% of JavaScript online submissions for Construct String from Binary Tree.
+ * Memory Usage: 47.3 MB, less than 37.58% of JavaScript online submissions for Construct String from Binary Tree.
+ */
+var tree2str = function(root) {
+    
+    let str = ""
+    
+    trav(root)
+    
+    function trav(node){
+        if (node === null) return str += "()"
+        str += node.val
+        if (node.left === null){
+            if (node.right === null) return
+            str += "()("
+            trav(node.right)
+        }
+        else if (node.right === null){
+            str += "("
+            trav(node.left)
+        }
+        else {
+            str += "("
+            trav(node.left) 
+            str += ")("
+            trav(node.right)
+        }
+        str += ")"
+        
+        return 
+    }
+        
+        return str
+    
+};
