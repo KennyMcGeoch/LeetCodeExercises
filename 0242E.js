9b85405633d9ba6022509219e5f341ec629f122f
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-12-16 14:23:07 +0000

    Improved solution with hash

--- 0242E.js
+++ 0242E.js
@@ -14,3 +14,26 @@ Memory Usage: 49.7 MB, less than 6.61% of JavaScript online submissions for Vali
     return false
     
 };
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ * Runtime: 56 ms, faster than 95.45% of JavaScript online submissions for Valid Anagram.
+ * Memory Usage: 43.1 MB, less than 77.25% of JavaScript online submissions for Valid Anagram.
+ */
+var isAnagram = function(s, t) {
+    
+    let hash = {}
+    for (let i=0; i<s.length; i++){
+        if (hash[s[i]] === undefined) hash[s[i]] = 1
+        else hash[s[i]]++
+    }
+    for (let i=0; i<t.length; i++){
+        if (hash[t[i]] === undefined) return false
+        else if (--hash[t[i]] === 0) delete hash[t[i]]
+    }
+    
+    return Object.keys(hash).length === 0
+    
+};
