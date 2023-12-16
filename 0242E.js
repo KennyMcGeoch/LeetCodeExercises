98fcce18d4375dcd122e39670d8aa81cd2fdc4fc
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-12-16 14:25:46 +0000

    Added a short circuit

--- 0242E.js
+++ 0242E.js
@@ -37,3 +37,28 @@ var isAnagram = function(s, t) {
     return Object.keys(hash).length === 0
     
 };
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ * Runtime: 54 ms, faster than 97.14% of JavaScript online submissions for Valid Anagram.
+ * Memory Usage: 42.6 MB, less than 87.09% of JavaScript online submissions for Valid Anagram.
+ */
+var isAnagram = function(s, t) {
+    
+    if (s.length !== t.length) return false
+    
+    let hash = {}
+    for (let i=0; i<s.length; i++){
+        if (hash[s[i]] === undefined) hash[s[i]] = 1
+        else hash[s[i]]++
+    }
+    for (let i=0; i<t.length; i++){
+        if (hash[t[i]] === undefined) return false
+        else if (--hash[t[i]] === 0) delete hash[t[i]]
+    }
+    
+    return Object.keys(hash).length === 0
+    
+};
