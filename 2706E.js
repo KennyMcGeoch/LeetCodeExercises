825db48c483729e7082da5b33d728c57b01a1248
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-12-20 10:06:34 +0000

    Added version without sorting

--- 2706E.js
+++ 2706E.js
@@ -15,3 +15,36 @@ var buyChoco = function(prices, money) {
     return money - cost
     
 };
+
+/**
+ * @param {number[]} prices
+ * @param {number} money
+ * @return {number}
+ * Runtime: 72 ms, faster than 97.04% of JavaScript online submissions for Buy Two Chocolates.
+ * Memory Usage: 46 MB, less than 80.00% of JavaScript online submissions for Buy Two Chocolates.
+ */
+var buyChoco = function(prices, money) {
+    
+    let min = prices[0]
+    let minS
+    
+    if (prices[1] < prices[0]){
+        minS = min
+        min = prices[1]
+    }
+    else minS = prices[1]
+    
+    for (let i=2; i<prices.length; i++){
+        if (prices[i] < minS){
+            if (prices[i] < min){
+                minS = min
+                min = prices[i]
+            }
+            else minS = prices[i]
+        }
+    }
+    if (money < min + minS) return money
+    
+    return money - min - minS
+    
+};
