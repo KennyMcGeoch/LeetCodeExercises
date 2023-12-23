663cf144dc7e921bc58d05741a569e332a27eb1a
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-12-23 12:32:54 +0000

    Added switch implementation instead of if elseif chain

--- 1496E.js
+++ 1496E.js
@@ -22,3 +22,43 @@ var isPathCrossing = function(path) {
     return false
     
 };
+
+/**
+ * @param {string} path
+ * @return {boolean}
+ * Runtime: 42 ms, faster than 96.40% of JavaScript online submissions for Path Crossing.
+ * Memory Usage: 42.1 MB, less than 53.24% of JavaScript online submissions for Path Crossing.
+ */
+var isPathCrossing = function(path) {
+    
+    let hash = { '0,0': true}
+    let curr = [0,0]
+    
+    for (let i=0; i<path.length; i++){
+        switch(path[i]){
+            case "N": {
+                curr[0]++
+                break
+            }
+                case "S": {
+                curr[0]--
+                break
+            }
+                case "E": {
+                curr[1]++
+                break
+            }
+                case "W": {
+                curr[1]--
+                break
+            }
+            default: break
+        }
+        
+        if (hash[curr] === true) return true
+        else hash[curr] = true
+    }
+    
+    return false
+    
+};
