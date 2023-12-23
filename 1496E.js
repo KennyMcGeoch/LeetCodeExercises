056ee65b9252e98c051dc93db6a46eb573a56374
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-12-23 12:26:04 +0000

    1496E

---
+++ 1496E.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} path
+ * @return {boolean}
+ * Runtime: 42 ms, faster than 96.40% of JavaScript online submissions for Path Crossing.
+ * Memory Usage: 42.1 MB, less than 53.24% of JavaScript online submissions for Path Crossing.
+ */
+var isPathCrossing = function(path) {
+    
+    let hash = { '0,0': true}
+    let curr = [0,0]
+    
+    for (let i=0; i<path.length; i++){
+        if (path[i] === "N") curr[0]++
+        else if (path[i] === "S") curr[0]--
+        else if (path[i] === "E") curr[1]++
+        else curr[1]--
+        
+        if (hash[curr] === true) return true
+        else hash[curr] = true
+    }
+    
+    return false
+    
+};
--- README.md
+++ README.md
@@ -299,6 +299,7 @@ Full List of Completed Projects in Numerical Order:
 1484. Group Sold Products By The Date (Easy)
 1491. Average Salary Excluding the Minimum and Maximum Salary (Easy)
 1493. Longest Subarray of 1's After Deleting One Element (Medium)
+1496. Path Crossing (Easy)
 1502. Can Make Arithmetic Progression From Sequence (Easy)
 1503. Last Moment Before All Ants Fall Out of a Plank (Medium)
 1512. Number of Good Pairs (Easy)
