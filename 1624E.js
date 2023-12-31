022685bd6a8c3ac2eee6a3a5453d7da06a8002a4
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-12-31 13:42:01 +0000

    Removed a variable and merged loops

--- 1624E.js
+++ 1624E.js
@@ -23,3 +23,25 @@ var maxLengthBetweenEqualCharacters = function(s) {
     return min - 1 
     
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ * Runtime: 56 ms, faster than 55.56% of JavaScript online submissions for Largest Substring Between Two Equal Characters.
+ * Memory Usage: 42.3 MB, less than 72.22% of JavaScript online submissions for Largest Substring Between Two Equal Characters.
+ */
+var maxLengthBetweenEqualCharacters = function(s) {
+    
+    let first = {}
+    let min = 0
+    
+    for (let i=0; i<s.length; i++){
+        if (first[s[i]] !== undefined){
+            min = Math.max(min, i - first[s[i]])
+        }
+        else first[s[i]] = i
+    }
+    
+    return min - 1 
+    
+};
