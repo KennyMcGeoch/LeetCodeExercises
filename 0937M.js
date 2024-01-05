9aafeeb9533d82952748d6ec65d38448ef3dcee7
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-01-05 11:38:03 +0000

    Replaced seatch with indexOf to compare performance

--- 0937M.js
+++ 0937M.js
@@ -29,3 +29,35 @@ var reorderLogFiles = function(logs) {
     return word.concat(num)
     
 };
+
+/**
+ * @param {string[]} logs
+ * @return {string[]}
+ * Runtime: 52 ms, faster than 93.87% of JavaScript online submissions for Reorder Data in Log Files.
+ * Memory Usage: 47.1 MB, less than 39.15% of JavaScript online submissions for Reorder Data in Log Files.
+ */
+var reorderLogFiles = function(logs) {
+    
+    let num = []
+    let word = []
+    let hash = {}
+    
+    for (let i=0; i<logs.length; i++){
+        let temp = logs[i].indexOf(" ")
+        if (isNum(logs[i][temp+1]))num.push(logs[i])
+        else {
+            word.push(logs[i])
+            hash[logs[i]] = logs[i].slice(temp+1)
+            }
+    }
+    
+    function isNum(str){
+    return !/\D/.test(str);
+    }
+    
+    word.sort()
+    word.sort((a,b)=>hash[a].localeCompare(hash[b]))
+    
+    return word.concat(num)
+    
+};
