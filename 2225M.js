3f35a216f98bb0ecea5aa74373e6040ad39b97d2
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-01-15 07:18:24 +0000

    Added Set solution but it's less efficient than hashmap

--- 2225M.js
+++ 2225M.js
@@ -1,8 +1,8 @@
 /**
  * @param {number[][]} matches
  * @return {number[][]}
- * Runtime: 542 ms, faster than 83.56% of JavaScript online submissions for Find Players With Zero or One Losses.
- * Memory Usage: 117 MB, less than 48.95% of JavaScript online submissions for Find Players With Zero or One Losses.
+ * Runtime: 303 ms, faster than 90.97% of JavaScript online submissions for Find Players With Zero or One Losses.
+ * Memory Usage: 110.1 MB, less than 80.69% of JavaScript online submissions for Find Players With Zero or One Losses.
  */
  var findWinners = function(matches) {
     
@@ -18,6 +18,39 @@
     
     for (x in hash){
         if (hash[x] < 2)solution[hash[x]].push(x)
+    }
+    
+    return solution
+    
+};
+
+/**
+ * @param {number[][]} matches
+ * @return {number[][]}
+ * Runtime: 314 ms, faster than 77.88% of JavaScript online submissions for Find Players With Zero or One Losses.
+ * Memory Usage: 120.4 MB, less than 45.79% of JavaScript online submissions for Find Players With Zero or One Losses.
+ */
+var findWinners = function(matches) {
+    
+    let solution = [[],[]]
+    let hash = {}
+    let oneLoss = new Set()
+    let twoLoss = new Set()
+    let iterations = matches.length
+    
+    for (let i=0; i<iterations; i++){
+        hash[matches[i][0]] = true
+        hash[matches[i][1]] = true
+        if (oneLoss.has(matches[i][1])) twoLoss.add(matches[i][1])
+        else oneLoss.add(matches[i][1])
+    }
+    let arr = Object.keys(hash)
+    for (let i=0; i<arr.length; i++){
+        let temp = Number(arr[i])
+        if (oneLoss.has(temp)){
+            if (twoLoss.has(temp) === false)solution[1].push(temp)
+            }
+        else solution[0].push(temp)
     }
     
     return solution
