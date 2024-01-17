f45daf820ae79126a23b6e00b49e426c5090f531
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-01-17 17:34:10 +0000

    Added version without if statement

--- 1207E.js
+++ 1207E.js
@@ -1,8 +1,8 @@
 /**
  * @param {number[]} arr
  * @return {boolean}
- * Runtime: 111 ms, faster than 19.60% of JavaScript online submissions for Unique Number of Occurrences.
- * Memory Usage: 42.7 MB, less than 46.04% of JavaScript online submissions for Unique Number of Occurrences.
+ * Runtime: 49 ms, faster than 83.30% of JavaScript online submissions for Unique Number of Occurrences.
+ * Memory Usage: 42.9 MB, less than 43.17% of JavaScript online submissions for Unique Number of Occurrences.
  */
  var uniqueOccurrences = function(arr) {
     
@@ -17,4 +17,38 @@
     let hashArr = Object.values(hash)
     if (new Set(hashArr).size !== hashArr.length)return false
     return true    
+};
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    
+    let iterations = arr.length
+    let hash = {}
+    
+    for (i=0; i<iterations; i++)hash[arr[i]] = (hash[arr[i]] || 0) + 1
+    
+    let hashArr = Object.values(hash)
+
+    return new Set(hashArr).size === hashArr.length 
+};
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ * Runtime: 44 ms, faster than 95.66% of JavaScript online submissions for Unique Number of Occurrences.
+ * Memory Usage: 42.2 MB, less than 81.13% of JavaScript online submissions for Unique Number of Occurrences.
+ */
+var uniqueOccurrences = function(arr) {
+    
+    let iterations = arr.length
+    let hash = {}
+    
+    for (i=0; i<iterations; i++)hash[arr[i]] = (hash[arr[i]] || 0) + 1
+    
+    let hashArr = Object.values(hash)
+
+    return new Set(hashArr).size === hashArr.length 
 };
