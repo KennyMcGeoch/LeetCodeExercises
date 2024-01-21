9183b546ba122e8373f016e4290670bba897a0c8
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-01-21 19:02:50 +0000

    Added version with less space complexity

--- 0198M.js
+++ 0198M.js
@@ -20,3 +20,24 @@
     return Math.max(totalTheft[iterations-1],totalTheft[iterations-2])
     
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Runtime: 44 ms, faster than 91.16% of JavaScript online submissions for House Robber.
+ * Memory Usage: 48.8 MB, less than 5.18% of JavaScript online submissions for House Robber.
+ */
+var rob = function(nums) {
+
+    let iterations = nums.length
+
+    if (iterations === 1) return nums[0]
+    else if (iterations === 2) return Math.max(nums[0],nums[1])
+
+    for (let i=2; i<iterations; i++){
+        nums[i] += Math.max(nums[i-3] || 0, nums[i-2])
+    }
+     
+    return Math.max(nums[--iterations],nums[--iterations])
+    
+};
