5d2d873d38b132e1c9c0f1a055b7ad26e8b03965
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-02-05 15:07:42 +0000

    Optimised code so second pass goes through hash instead of full array

--- 0387E.js
+++ 0387E.js
@@ -27,3 +27,34 @@
     return -1
     
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ * Runtime: 77 ms, faster than 85.81% of JavaScript online submissions for First Unique Character in a String.
+ * Memory Usage: 53.6 MB, less than 16.31% of JavaScript online submissions for First Unique Character in a String.
+ */
+var firstUniqChar = function(s) {
+
+    let instances = {}
+    let iterations = s.length
+
+    for (i=0; i<iterations; i++){
+        if (instances[s[i]] === undefined){
+            instances[s[i]] = i
+        }
+        else{
+            instances[s[i]] = "a"
+        }
+    }
+    
+    let ans = Infinity
+    for (x in instances){
+        if (instances[x] !== "a") ans = Math.min(ans, instances[x])
+    }
+     
+     if (ans === Infinity) return -1
+
+    return ans
+    
+};
