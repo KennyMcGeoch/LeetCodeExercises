625ff78b9a503f91701a74a0a8d7bf1cfc0004d3
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-02-10 02:53:21 +0000

    Merged loops

--- 0647M.js
+++ 0647M.js
@@ -27,3 +27,31 @@ var countSubstrings = function(s) {
     return ans
     
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ * Runtime: 55 ms, faster than 89.27% of JavaScript online submissions for Palindromic Substrings.
+ * Memory Usage: 52.3 MB, less than 21.46% of JavaScript online submissions for Palindromic Substrings.
+ */
+var countSubstrings = function(s) {
+    
+    let ans = s.length
+    
+    for (let i=0; i<s.length; i++){
+        count(i-1,i+1)
+        count(i, i+1)
+    }
+    
+    function count(min,max){
+        while(min > -1 && s[min] === s[max]){
+            ans++
+            min--
+            max++
+        }
+        return
+    }
+    
+    return ans
+    
+};
