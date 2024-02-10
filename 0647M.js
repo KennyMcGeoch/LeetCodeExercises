4e03157997a785caf1aee58912361a413330239b
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-02-10 02:51:14 +0000

    0647M

---
+++ 0647M.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ * Runtime: 55 ms, faster than 89.27% of JavaScript online submissions for Palindromic Substrings.
+ * Memory Usage: 51.4 MB, less than 23.17% of JavaScript online submissions for Palindromic Substrings.
+ */
+var countSubstrings = function(s) {
+    
+    let ans = s.length
+    
+    for (let i=0; i<s.length; i++){
+        count(i-1,i+1)
+    }
+    for (let i=0; i<s.length; i++){
+        count(i, i+1)
+    }
+    
+    function count(min,max){
+        while(min > -1 && s[min] === s[max]){
+            ans++
+            min--
+            max++
+        }
+        return
+    }
+    
+    return ans
+    
+};
--- README.md
+++ README.md
@@ -223,6 +223,7 @@ Full List of Completed Projects in Numerical Order:
 627. Swap Salary (Easy)
 633. Sum of Square Numbers (Medium)
 645. Set Mismatch (Easy)
+647. Palindromic Substrings (Medium)
 649. Dota2 Senate (Medium)
 653. Two Sum IV - Input is a BST (Easy)
 661. Image Smoother (Easy)
