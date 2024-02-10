6563b0d233ca954337e5a668574d5283540e1208
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-02-10 03:00:32 +0000

    Added short circuit for empty string and rearrange while conditionals

--- 0647M.js
+++ 0647M.js
@@ -55,3 +55,32 @@ var countSubstrings = function(s) {
     return ans
     
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ * Runtime: 52 ms, faster than 95.12% of JavaScript online submissions for Palindromic Substrings.
+ * Memory Usage: 51.2 MB, less than 25.36% of JavaScript online submissions for Palindromic Substrings.
+ */
+var countSubstrings = function(s) {
+    if (s.length === 0) return 0
+    
+    let ans = s.length
+    
+    for (let i=0; i<s.length; i++){
+        count(i-1,i+1)
+        count(i, i+1)
+    }
+    
+    function count(min,max){
+        while(s[min] === s[max] && min > -1){
+            ans++
+            min--
+            max++
+        }
+        return
+    }
+    
+    return ans
+    
+};
