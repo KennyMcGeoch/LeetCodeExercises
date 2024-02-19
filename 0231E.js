7863645cd5fd1499e6c53422c369c9e6bcf18f14
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-02-19 01:38:35 +0000

    Added versions using log and bit manipulation

--- 0231E.js
+++ 0231E.js
@@ -12,3 +12,28 @@
     }
     return true
 };
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ * Runtime: 69 ms, faster than 60.52% of JavaScript online submissions for Power of Two.
+Memory Usage: 53.3 MB, less than 17.88% of JavaScript online submissions for Power of Two.
+ */
+var isPowerOfTwo = function(n) {
+        
+    return (Math.log(n) / Math.log(2)).toFixed(10) % 1 === 0
+};
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ * Runtime: 76 ms, faster than 39.73% of JavaScript online submissions for Power of Two.
+ * Memory Usage: 53.8 MB, less than 6.08% of JavaScript online submissions for Power of Two.
+ */
+var isPowerOfTwo = function(n) {
+        
+    n = n.toString(2)
+    for (let i = 1; i<n.length; i++) if (n[i] === "1") return false
+    
+    return n > 0
+};
