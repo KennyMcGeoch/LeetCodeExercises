4898a504273bfa6c6bc5026dc87b7cf66761ee7e
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-02-20 12:44:15 +0000

    Added sumation solution

--- 0268E.js
+++ 0268E.js
@@ -17,3 +17,34 @@ Memory Usage: 44.6 MB, less than 37.20% of JavaScript online submissions for Mis
     return
     
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Runtime: 73 ms, faster than 33.30% of JavaScript online submissions for Missing Number.
+ * Memory Usage: 51.2 MB, less than 32.66% of JavaScript online submissions for Missing Number.
+ */
+var missingNumber = function(nums) {
+
+    nums.sort((a,b)=>a-b)
+
+    for (i=0; i> -1; i++){
+        if (nums[i] !== i){
+            return i
+        }
+    }
+    
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Runtime: 56 ms, faster than 75.42% of JavaScript online submissions for Missing Number.
+ * Memory Usage: 51.3 MB, less than 27.97% of JavaScript online submissions for Missing Number.
+ */
+var missingNumber = function(nums) {
+
+    let total = nums.reduce((a,b)=>a+b)
+    return (nums.length) * ((nums.length+1)/2) - total
+    
+};
