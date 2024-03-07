2a27fd51b39a178304736eab54bedc0444a7692c
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-03-07 10:18:17 +0000

    Added 2 pointer solution

--- 0876E.js
+++ 0876E.js
@@ -10,6 +10,8 @@
  * @return {ListNode}
  * Runtime: 99 ms, faster than 34.95% of JavaScript online submissions for Middle of the Linked List.
  * Memory Usage: 42.3 MB, less than 26.81% of JavaScript online submissions for Middle of the Linked List.
+ * Runtime: 50 ms, faster than 65.74% of JavaScript online submissions for Middle of the Linked List.
+ * Memory Usage: 48.9 MB, less than 37.27% of JavaScript online submissions for Middle of the Linked List.
  */
  var middleNode = function(head) {
 
@@ -31,6 +33,31 @@
     return solution    
 };
 
-middleNode({
-    val: 1,
-    next: {val:2, next :{val:3, next : {val: 4, next:{ val:5, next :null}}}}})
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ * Runtime: 46 ms, faster than 84.75% of JavaScript online submissions for Middle of the Linked List.
+ * Memory Usage: 49.2 MB, less than 9.66% of JavaScript online submissions for Middle of the Linked List.
+ */
+var middleNode = function(head) {
+
+    let fast = head
+    
+    while (true){
+        if (fast.next === null) return head
+        if (fast.next.next === null) return head.next
+        head = head.next
+        fast = fast.next.next
+    }
+     
+    return
+     
+    
+};
