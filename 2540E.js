43adb6ff2c72d426a6f5d3130e478b55aea586f8
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-03-09 10:45:10 +0000

    2540E

--- 2540E.js
+++ 2540E.js
@@ -42,3 +42,23 @@ var getCommon = function(nums1, nums2) {
     
 };
 
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ * Runtime: 61 ms, faster than 90.82% of JavaScript online submissions for Minimum Common Value.
+ * Memory Usage: 57.5 MB, less than 65.05% of JavaScript online submissions for Minimum Common Value.
+ */
+var getCommon = function(nums1, nums2) {
+    
+    let left = 0
+    let right = 0
+    
+    while(true){
+        if (nums1[left] === nums2[right]) return nums1[left]
+        else if (nums1[left] > nums2[right]) right++
+        else if (nums1[left] < nums2[right]) left++
+        else return -1
+    }
+    
+};
