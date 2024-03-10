5276d0851bd8a88149fab1d12f59032082cdf7dc
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-03-10 13:25:11 +0000

    Added Set solution

--- 0349E.js
+++ 0349E.js
@@ -4,6 +4,8 @@
  * @return {number[]}
  * Runtime: 100 ms, faster than 51.89% of JavaScript online submissions for Intersection of Two Arrays.
  * Memory Usage: 42.4 MB, less than 86.60% of JavaScript online submissions for Intersection of Two Arrays.
+ * Runtime: 44 ms, faster than 96.87% of JavaScript online submissions for Intersection of Two Arrays.
+ * Memory Usage: 49.1 MB, less than 71.02% of JavaScript online submissions for Intersection of Two Arrays.
  */
  var intersection = function(nums1, nums2) {
 
@@ -19,4 +21,24 @@
     return solution  
 };
 
-intersection([1,2,2,2,1],[2,2,2,1])
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ * Runtime: 42 ms, faster than 98.43% of JavaScript online submissions for Intersection of Two Arrays.
+ * Memory Usage: 50.8 MB, less than 24.85% of JavaScript online submissions for Intersection of Two Arrays.
+ */
+var intersection = function(nums1, nums2) {
+
+    let solution = []
+    let arrOne = new Set()
+    let arrTwo = new Set()
+    for (let i=0; i<nums1.length; i++) arrOne.add(nums1[i])
+    for (let i=0; i<nums2.length; i++) arrTwo.add(nums2[i])
+    arrOne.forEach((a)=> {
+        if (arrTwo.has(a))solution.push(a)
+    })
+
+    return solution  
+};
+
