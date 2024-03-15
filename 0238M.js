ee74864a921fd27f185772142763aec878d2ccf0
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-03-15 12:17:25 +0000

    Added 0(1) Space solution

--- 0238M.js
+++ 0238M.js
@@ -22,3 +22,24 @@ var productExceptSelf = function(nums) {
     return ans
     
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * Runtime: 77 ms, faster than 90.44% of JavaScript online submissions for Product of Array Except Self.
+ * Memory Usage: 59.2 MB, less than 85.36% of JavaScript online submissions for Product of Array Except Self.
+ */
+var productExceptSelf = function(nums) {
+    
+    let ans = [1]
+    
+    for (let i=0; i<nums.length-1; i++) ans.push(ans[i] * nums[i])
+    let sum = 1
+    
+    for (let i=nums.length-1; i>-1; i--){
+        ans[i] *= (sum)
+        sum *= nums[i]
+    }
+    
+    return ans    
+};
