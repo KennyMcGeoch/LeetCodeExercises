97e690ec98f23727b048df12df3e8eb117ad63aa
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-03-17 00:36:22 +0000

    0057M

--- 0057M.js
+++ 0057M.js
@@ -4,6 +4,8 @@
  * @return {number[][]}
  * Runtime: 136 ms, faster than 22.07% of JavaScript online submissions for Insert Interval.
  * Memory Usage: 43.6 MB, less than 98.81% of JavaScript online submissions for Insert Interval.
+ * Runtime: 73 ms, faster than 67.58% of JavaScript online submissions for Insert Interval.
+ * Memory Usage: 54 MB, less than 67.42% of JavaScript online submissions for Insert Interval.
  */
  var insert = function(intervals, newInterval) {
 
@@ -26,4 +28,30 @@
     solution.sort((a,b)=>a[0]-b[0])
 
     return solution    
+};
+
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ * Runtime: 64 ms, faster than 92.59% of JavaScript online submissions for Insert Interval.
+ * Memory Usage: 53.7 MB, less than 78.32% of JavaScript online submissions for Insert Interval.
+ */
+var insert = function(intervals, newInterval) {
+
+    let ans = []
+    
+    intervals.push(newInterval)
+    intervals.sort((a,b)=>a[0]-b[0])
+    
+     for (let i=0; i<intervals.length; i++){
+         let min = intervals[i][0]
+         let max = intervals[i][1]
+         while(i < intervals.length-1 && max >= intervals[i+1][0]){
+             max = Math.max(max, intervals[++i][1])
+         }
+         ans.push([min,max])
+     }
+
+     return ans    
 };
