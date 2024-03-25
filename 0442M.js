9aac5435fe50e1b3d20201e526eb3511a3a2c0e6
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-03-25 15:12:01 +0000

    0442M

--- 0442M.js
+++ 0442M.js
@@ -22,3 +22,22 @@ Memory Usage: 52 MB, less than 50.72% of JavaScript online submissions for Find 
     return solution
     
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * Runtime: 77 ms, faster than 85.58% of JavaScript online submissions for Find All Duplicates in an Array.
+ * Memory Usage: 59.1 MB, less than 63.32% of JavaScript online submissions for Find All Duplicates in an Array.
+ */
+var findDuplicates = function(nums) {
+
+    let ans = new Array(nums.length).fill(0)
+     
+    for (i=0; i< nums.length; i++){
+        if (ans[nums[i]-1] === 0) ans[nums[i]-1] -= nums[i]
+        else ans[nums[i]-1] *= -1
+    }
+
+    return ans.filter((a)=>a>0)
+    
+};
