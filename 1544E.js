297a9fb1f2374cb9751699807dd80d03475dac3a
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-04-05 10:25:45 +0100

    1544E

--- 1544E.js
+++ 1544E.js
@@ -22,3 +22,25 @@
     return s
     
 };
+
+/**
+ * @param {string} s
+ * @return {string}
+ * Runtime: 48 ms, faster than 95.27% of JavaScript online submissions for Make The String Great.
+ * Memory Usage: 49.7 MB, less than 98.65% of JavaScript online submissions for Make The String Great.
+ */
+var makeGood = function(s) {
+
+    let stack = []
+    
+    for (let i=0; i<s.length; i++){
+        if (stack.length){
+            if (Math.abs(stack[stack.length-1].charCodeAt(0) - s.charCodeAt(i))=== 32) stack.pop()
+            else stack.push(s[i])
+        }
+        else stack.push(s[i])
+    }
+
+    return stack.join("")
+    
+};
