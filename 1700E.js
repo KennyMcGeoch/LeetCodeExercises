068cb6efa0337314c3b0e23a1e6d1c046d1eebec
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-04-08 13:55:43 +0100

    Optimised variable initialisation

--- 1700E.js
+++ 1700E.js
@@ -38,3 +38,24 @@ var countStudents = function(students, sandwiches) {
     
     return total
 };
+
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ * Runtime: 41 ms, faster than 98.73% of JavaScript online submissions for Number of Students Unable to Eat Lunch.
+ * Memory Usage: 48.8 MB, less than 71.90% of JavaScript online submissions for Number of Students Unable to Eat Lunch.
+ */
+var countStudents = function(students, sandwiches) {
+    
+    let stud = []
+    let total = students.length
+    stud[1] = students.reduce((a,b)=>a+b)
+    stud[0] = students.length - stud[1]
+    for (let i=0; i<sandwiches.length; i++){
+        if (stud[sandwiches[i]]-- === 0) return total
+        else total--
+    }
+    
+    return total
+};
