63b543a648ffb31acd852c7d937c21c2a9df0161
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-04-15 17:16:53 +0100

    Added UNION solution instead of subquery solution

--- 1789E.sql
+++ 1789E.sql
@@ -4,3 +4,9 @@
 
 SELECT employee_id, department_id FROM Employee WHERE primary_flag = 'Y' OR employee_id IN
 (SELECT employee_id FROM Employee GROUP BY employee_id HAVING COUNT(employee_id) = 1)
+
+# Write your MySQL query statement below
+# Runtime: 1094 ms, faster than 52.35% of MySQL online submissions for Primary Department for Each Employee.
+# Memory Usage: 0B, less than 100.00% of MySQL online submissions for Primary Department for Each Employee.
+
+SELECT employee_id, department_id FROM Employee WHERE primary_flag = 'Y' UNION SELECT employee_id, department_id FROM Employee GROUP BY employee_id HAVING COUNT(employee_id) = 1
