ea627196ca95b4575f557d19defa3ae776dbccb6
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-05-16 11:40:27 +0100

    Optimised comparisons

--- 2331E.js
+++ 2331E.js
@@ -19,3 +19,25 @@ var evaluateTree = function(root) {
     else return evaluateTree(root.left) && evaluateTree(root.right)
     
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ * Runtime: 54 ms, faster than 90.48% of JavaScript online submissions for Evaluate Boolean Binary Tree.
+ * Memory Usage: 55.2 MB, less than 79.37% of JavaScript online submissions for Evaluate Boolean Binary Tree.
+ */
+var evaluateTree = function(root) {
+    
+    if (root.val < 2) return root.val
+    else if (root.val === 2) return evaluateTree(root.left) || evaluateTree(root.right)
+    else return evaluateTree(root.left) && evaluateTree(root.right)
+    
+};
