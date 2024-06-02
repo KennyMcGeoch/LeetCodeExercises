e99131e186613f48b90c99a816df36e86a1f4929
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-06-02 21:17:51 +0100

    0344E

--- 0344E.js
+++ 0344E.js
@@ -1,10 +1,28 @@
 /**
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
- * Runtime: 146 ms, faster than 51.19% of JavaScript online submissions for Reverse String.
-Memory Usage: 50.1 MB, less than 11.68% of JavaScript online submissions for Reverse String.
+ * Runtime: 75 ms, faster than 71.78% of JavaScript online submissions for Reverse String.
+ * Memory Usage: 56.7 MB, less than 77.86% of JavaScript online submissions for Reverse String.
  */
 var reverseString = function(s) {  
     s.reverse(); // couldn't believe it was just 1 line of code
+    
+};
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ * Runtime: 75 ms, faster than 71.78% of JavaScript online submissions for Reverse String.
+ * Memory Usage: 56.8 MB, less than 72.81% of JavaScript online submissions for Reverse String.
+ */
+var reverseString = function(s) {
+    let left = 0
+    let right = s.length-1
+    
+    while (left < right){
+        let temp = s[left]
+        s[left++] = s[right]
+        s[right--] = temp
+    }
     
 };
