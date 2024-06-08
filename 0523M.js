c8eaa9bcc0610db0ae3c4386cc498a490a7fca5f
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-06-08 15:04:20 +0100

    Removed variable

--- 0523M.js
+++ 0523M.js
@@ -26,3 +26,31 @@
     
 };
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ * Runtime: 106 ms, faster than 66.43% of JavaScript online submissions for Continuous Subarray Sum.
+ * Memory Usage: 72.1 MB, less than 80.95% of JavaScript online submissions for Continuous Subarray Sum.
+ */
+var checkSubarraySum = function(nums, k) {
+
+    let total = nums[0]
+    let iterations = nums.length
+    let remainder = {}
+
+    remainder[nums[0] % k] = 0
+
+    for (i=1; i<iterations; i++){
+        total += nums[i]
+        total %= k
+        if (total === 0) return true
+        if (remainder[total] !== undefined){
+            if (remainder[total]+1 < i)return true
+        }
+        else remainder[total] = i 
+    }
+    return false
+    
+};
+
