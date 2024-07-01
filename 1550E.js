2c6aa6835cb50e09b8ae07fd8c54d12b3f123f06
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-07-01 12:54:26 +0100

    1550E

--- 1550E.js
+++ 1550E.js
@@ -3,6 +3,8 @@
  * @return {boolean}
  * Runtime: 99 ms, faster than 39.84% of JavaScript online submissions for Three Consecutive Odds.
  * Memory Usage: 42.6 MB, less than 14.06% of JavaScript online submissions for Three Consecutive Odds.
+ * Runtime: 38 ms, faster than 99.49% of JavaScript online submissions for Three Consecutive Odds.
+ * Memory Usage: 48.4 MB, less than 96.94% of JavaScript online submissions for Three Consecutive Odds.
  */
  var threeConsecutiveOdds = function(arr) {
 
