363425c98acf6bde535b814ead7eb68a9dab2fbc
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-07-07 11:07:19 +0100

    3100M

--- 3100M.js
+++ 3100M.js
@@ -3,7 +3,7 @@
  * @param {number} numExchange
  * @return {number}
  * Runtime: 55 ms, faster than 91.11% of JavaScript online submissions for Water Bottles II.
-Memory Usage: 51.4 MB, less than 24.44% of JavaScript online submissions for Water Bottles II.
+ * Memory Usage: 51.4 MB, less than 24.44% of JavaScript online submissions for Water Bottles II.
  */
 var maxBottlesDrunk = function(numBottles, numExchange) {
     let ans = numBottles
