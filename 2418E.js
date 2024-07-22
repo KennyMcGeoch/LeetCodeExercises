9c1cfbe8c5d6678dbab0f4534c5edf9b1ee22e47
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-07-22 11:10:00 +0100

    2418E

--- 2418E.js
+++ 2418E.js
@@ -19,3 +19,24 @@
     }
     return solution
     };
+
+    /**
+ * @param {string[]} names
+ * @param {number[]} heights
+ * @return {string[]}
+ * Runtime: 77 ms, faster than 47.53% of JavaScript online submissions for Sort the People.
+ * Memory Usage: 53.1 MB, less than 92.68% of JavaScript online submissions for Sort the People.
+ */
+var sortPeople = function(names, heights) {
+    for (let i=1; i< names.length; i++){
+        while(i > 0 && heights[i] > heights[i-1]){
+            let heightTemp = heights[i-1]
+            let namesTemp = names[i-1]
+            heights[i-1] = heights[i]
+            heights[i] = heightTemp
+            names[i-1] = names[i]
+            names[i--] = namesTemp            
+        }
+    }
+    return names
+};
