65dce000862ae9dbe149d8c80590ab72ab0f899a
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-08-09 11:11:35 +0100

    0840M

--- 0840M.js
+++ 0840M.js
@@ -81,3 +81,45 @@ var numMagicSquaresInside = function(grid) {
     return ans
     
 };
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ * Runtime: 42 ms, faster than 100.00% of JavaScript online submissions for Magic Squares In Grid.
+ * Memory Usage: 48.7 MB, less than 100.00% of JavaScript online submissions for Magic Squares In Grid.
+ */
+var numMagicSquaresInside = function(grid) {
+    
+    let ans = 0
+    
+    for (let i=1; i<grid.length-1; i++){
+        for (let j=1; j<grid[0].length-1; j++){
+            if (grid[i][j] !== 5)continue
+
+            if (grid[i-1][j] + grid[i+1][j] + grid[i][j] !== 15) continue
+            if (grid[i-1][j+1] + grid[i+1][j+1] + grid[i][j+1] !== 15) continue
+            if (grid[i-1][j-1] + grid[i+1][j-1] + grid[i][j-1] !== 15) continue
+            if (grid[i][j+1] + grid[i][j-1] + grid[i][j] !== 15) continue
+            if (grid[i+1][j+1] + grid[i+1][j-1] + grid[i+1][j] !== 15) continue
+            if (grid[i-1][j+1] + grid[i-1][j-1] + grid[i-1][j] !== 15) continue
+            if (grid[i-1][j-1] + grid[i+1][j+1] + grid[i][j] !== 15) continue
+            if (grid[i-1][j+1] + grid[i+1][j-1] + grid[i][j] !== 15) continue
+            
+            let nums = {}
+            nums[grid[i-1][j]] = true
+            nums[grid[i][j]] = true
+            nums[grid[i+1][j]] = true
+            nums[grid[i-1][j-1]] = true
+            nums[grid[i][j-1]] = true
+            nums[grid[i+1][j-1]] = true
+            nums[grid[i-1][j+1]] = true
+            nums[grid[i][j+1]] = true
+            nums[grid[i+1][j+1]] = true
+            
+            if (nums[1] && nums[2] && nums[3] && nums[4] && nums[5] && nums[6] && nums[7] && nums[8] && nums[9] )ans++
+        }
+    }
+    
+    return ans
+    
+};
