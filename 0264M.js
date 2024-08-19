322dad66d188882b4d7b7d78f747c2860f507381
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-08-19 10:31:31 +0100

    0264M

--- 0264M.js
+++ 0264M.js
@@ -24,3 +24,30 @@
     return uglyArr[n-1]
     
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ * Runtime: 93 ms, faster than 20.17% of JavaScript online submissions for Ugly Number II.
+ * Memory Usage: 55.1 MB, less than 20.17% of JavaScript online submissions for Ugly Number II.
+ */
+let uglyArr = [1]
+    let twoX = 0
+    let threeX = 0
+    let fiveX = 0
+
+    for (i=0; i<1690; i++){
+        twoX = uglyArr[i] * 2
+        threeX = uglyArr[i] * 3
+        fiveX = uglyArr[i] * 5
+        if (uglyArr.includes(twoX) === false)uglyArr.push(twoX)
+        if (uglyArr.includes(threeX) === false)uglyArr.push(threeX)
+        if (uglyArr.includes(fiveX) === false)uglyArr.push(fiveX)
+        uglyArr.sort((a,b)=>a-b)
+    }
+
+ var nthUglyNumber = function(n) {
+
+    return uglyArr[n-1]
+    
+};
