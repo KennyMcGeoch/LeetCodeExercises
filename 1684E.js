4603b284c9915aff07bc9889c6159701aa8e1a60
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-09-12 18:25:55 +0100

    1684E

--- 1684E.js
+++ 1684E.js
@@ -22,3 +22,30 @@ var countConsistentStrings = function(allowed, words) {
     return ans
     
 };
+
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ * Runtime: 73 ms, faster than 93.53% of JavaScript online submissions for Count the Number of Consistent Strings.
+ * Memory Usage: 59.2 MB, less than 41.32% of JavaScript online submissions for Count the Number of Consistent Strings.
+ */
+var countConsistentStrings = function(allowed, words) {
+    
+    let ans = 0
+    let allowedSet = new Set()
+    
+    for (let i=0; i<allowed.length; i++) allowedSet.add(allowed[i])
+    
+    for (let i=0; i<words.length; i++){
+        ans += check(words[i])
+    }
+    
+    function check(str){
+        for (let j=0; j<str.length; j++)if (allowedSet.has(str[j]) === false) return 0
+        return 1
+    }
+    
+    return ans
+    
+};
