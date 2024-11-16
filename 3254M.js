67dfd91baa329ae36d0ebef5ba34bc5c981f8826
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-11-16 22:45:12 +0000

    3254M

--- 3254M.js
+++ 3254M.js
@@ -21,3 +21,23 @@ var resultsArray = function(nums, k) {
     }
     return ans
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ * Runtime: 1 ms, faster than 100.00% of JavaScript online submissions for Find the Power of K-Size Subarrays I.
+ * Memory Usage: 52.8 MB, less than 64.86% of JavaScript online submissions for Find the Power of K-Size Subarrays I.
+ */
+var resultsArray = function(nums, k) {
+    if (k === 1) return nums
+    let ans = []
+    let count = 0
+    for (let i=0; i<nums.length; i++){
+        if (nums[i] === nums[i-1] + 1) count++
+        else count = 1
+        if (count >= k) ans.push(nums[i])
+        else if (i > k-2) ans.push(-1)
+    }
+    return ans
+};
