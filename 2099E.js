099bc25c9247f465f39c7b962fcf6bb6a1a6a84b
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2025-06-28 23:38:43 +0100

    2099E

--- 2099E.js
+++ 2099E.js
@@ -18,4 +18,38 @@ Memory Usage: 44.2 MB, less than 57.53% of JavaScript online submissions for Fin
     return filteredArray
 };
 
-maxSubsequence([-1,-2,3,4,3,3],3)
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ * Runtime 4ms Beats 89.26%
+ * Memory 58.39MB Beats 17.36%
+ */
+var maxSubsequence = function(nums, k) {
+
+    let ans = []
+    let arr = new Array(nums.length)
+    for (let i=0; i<arr.length; i++) arr[i] = i
+    arr.sort((a,b)=>nums[a] - nums[b])
+    while (ans.length < k) ans.push(arr.pop())
+    ans.sort((a,b)=>a-b)
+    for (let i=0; i<ans.length; i++) ans[i] = nums[ans[i]]
+    return ans
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ * Runtime 2ms Beats99.17%
+ * Memory 57.00MB Beats 67.77%
+ */
+var maxSubsequence = function(nums, k) {
+
+    let ans = []
+    let arr = new Array(nums.length)
+    for (let i=0; i<arr.length; i++) arr[i] = i
+    arr.sort((a,b)=>nums[b] - nums[a])
+    for (let i=0; i<k; i++) ans[arr[i]] = nums[arr[i]]
+    return ans.filter((a)=>a !== undefined)
+};
