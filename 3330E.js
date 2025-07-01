be797e53019440045dbf1e20e8f30a0705e74a89
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2025-07-01 15:43:54 +0100

    Removed variable

--- 3330E.js
+++ 3330E.js
@@ -17,3 +17,18 @@ var possibleStringCount = function(word) {
 
     return ans + curr
 };
+
+/**
+ * @param {string} word
+ * @return {number}
+ * Runtime 34ms Beats 98.00%
+ * Memory 53.34MB Beats 96.00%
+ */
+var possibleStringCount = function(word) {
+    let ans = 1
+    for (let i=1; i<word.length; i++){
+        if (word[i-1] === word[i]) ans++
+    }
+
+    return ans
+};
