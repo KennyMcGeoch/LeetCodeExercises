799aa7a0afde75a1dd8dca2c7aa5294a81b9d5b5
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2025-07-01 15:40:47 +0100

    3330E

---
+++ 3330E.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word
+ * @return {number}
+ * Runtime 34ms Beats 98.00%
+ * Memory 53.48MB Beats 94.00%
+ */
+var possibleStringCount = function(word) {
+    let ans = 1
+    let curr = 0
+    for (let i=1; i<word.length; i++){
+        if (word[i-1] === word[i]) curr++
+        else{
+            ans += curr
+            curr = 0 
+        }
+    }
+
+    return ans + curr
+};
