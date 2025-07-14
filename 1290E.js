8a6d58ed42ba21f96569dae1260b2becf7478fd2
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2025-07-14 13:59:46 +0100

    1290E

--- 1290E.js
+++ 1290E.js
@@ -10,6 +10,8 @@
  * @return {number}
  * Runtime: 111 ms, faster than 16.39% of JavaScript online submissions for Convert Binary Number in a Linked List to Integer.
  * Memory Usage: 41.9 MB, less than 72.10% of JavaScript online submissions for Convert Binary Number in a Linked List to Integer.
+ * Runtime 0ms Beats100.00%
+ * Memory 53.27MB Beats 79.19%
  */
  var getDecimalValue = function(head) {
 
@@ -28,4 +30,27 @@
         count += (Math.pow(2,i) * solution[i])
     }
     return count
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ * Runtime 0ms Beats 100.00%
+ * Memory 53.48MB Beats 66.46%
+ */
+var getDecimalValue = function(head) {
+
+    let ans = ""
+    while(head){
+        ans += head.val
+        head = head.next
+    }
+    return parseInt(ans,2)
 };
