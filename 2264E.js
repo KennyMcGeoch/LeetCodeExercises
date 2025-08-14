c7f19c4c06d89f1da9ea3db1ae06d819b16b1026
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2025-08-14 21:54:08 +0100

    2264E

--- 2264E.js
+++ 2264E.js
@@ -21,3 +21,30 @@ var largestGoodInteger = function(num) {
     return ans.toString()
     
 };
+
+/**
+ * @param {string} num
+ * @return {string}
+ * Runtime 0ms Beats 100.00%
+ * Memory 83.72MB Beats 83.15%
+ */
+var largestGoodInteger = function(num) {
+    
+    let max = -1
+    let curr = num[0]
+    let len = 1
+    
+    for (let i=1; i<num.length; i++){
+        if (num[i] <= max) len = 0
+        else if(num[i] === curr){
+            len++
+            if (len === 3) max = num[i]
+        }
+        else len = 1
+        curr = num[i]
+    }
+    if (max === -1) return ""
+    
+    return max + max + max
+    
+};
