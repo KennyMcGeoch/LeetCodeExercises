9b34bf08ee51c19937d770151e3415a9e3a6804b
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2025-08-19 22:21:04 +0100

    2348M

--- 2348M.js
+++ 2348M.js
@@ -3,6 +3,8 @@
  * @return {number}
  * Runtime: 92 ms, faster than 67.07% of JavaScript online submissions for Number of Zero-Filled Subarrays.
  * Memory Usage: 53.2 MB, less than 75.61% of JavaScript online submissions for Number of Zero-Filled Subarrays.
+ * Runtime 3ms Beats 94.79%
+ * Memory 66.38MB Beats 86.46%
  */
 var zeroFilledSubarray = function(nums) {
     let total = 0
@@ -19,4 +21,22 @@ var zeroFilledSubarray = function(nums) {
     
     return total
     
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Runtime 2ms Beats 100.00%
+ * Memory 64.50MB Beats 98.96%
+ */
+var zeroFilledSubarray = function(nums) {
+    let ans = 0
+    let curr = 0
+    
+    for (let i=0; i<nums.length; i++){
+        if (nums[i]) curr = 0
+        else ans += ++curr
+    }
+
+    return ans    
 };
