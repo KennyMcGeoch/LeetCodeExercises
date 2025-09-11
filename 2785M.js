09dd4db18378e5974d59f2a4c5f76ff798139113
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2025-09-11 14:58:30 +0100

    2785M

--- 2785M.js
+++ 2785M.js
@@ -3,6 +3,8 @@
  * @return {string}
  * Runtime: 181 ms, faster than 76.38% of JavaScript online submissions for Sort Vowels in a String.
  * Memory Usage: 65.8 MB, less than 50.39% of JavaScript online submissions for Sort Vowels in a String.
+ * Runtime 72ms Beats 72.63%
+ * Memory 69.85MB Beats 65.26%
  */
 var sortVowels = function(s) {
     
@@ -20,4 +22,44 @@ var sortVowels = function(s) {
         else ans += s[i]
     }
     return ans
+};
+
+/**
+ * @param {string} s
+ * @return {string}
+ * Runtime 18ms Beats 98.95%
+ * Memory 62.54MB Beats 98.95%
+ */
+let arr = new Array(10).fill(0)
+let vArr = ["A","E","I","O","U","a","e","i","o","u"]
+let vowelSet = new Set(vArr)
+
+var sortVowels = function(s) {
+   
+   for (let i=0; i<s.length; i++){
+       if (vowelSet.has(s[i])){
+           if (s[i] === "A") arr[0]++
+           else if (s[i] === "E") arr[1]++
+           else if (s[i] === "I") arr[2]++
+           else if (s[i] === "O") arr[3]++
+           else if (s[i] === "U") arr[4]++
+           else if (s[i] === "a") arr[5]++
+           else if (s[i] === "e") arr[6]++
+           else if (s[i] === "i") arr[7]++
+           else if (s[i] === "o") arr[8]++
+           else arr[9]++
+       }   
+   }
+   let curr = 0
+   let ans = ""
+   for (let i=0; i<s.length; i++){
+       if (vowelSet.has(s[i])){
+           while(arr[curr] === 0) curr++
+           ans += vArr[curr]
+           arr[curr]--
+       }
+       else ans += s[i]
+   }
+
+   return ans
 };
