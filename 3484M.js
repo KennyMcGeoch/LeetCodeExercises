b862998bf019639f7bac309238e307e54deb0422
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2025-09-19 16:39:29 +0100

    3484M

--- 3484M.js
+++ 3484M.js
@@ -161,3 +161,108 @@ Spreadsheet.prototype.getValue = function(formula) {
  * obj.resetCell(cell)
  * var param_3 = obj.getValue(formula)
  */
+
+/**
+ * @param {number} rows
+ * Runtime 75ms Beats 80.56%
+ * Memory 78.64MB Beats 33.33%
+ */
+var Spreadsheet = function(rows) {
+    obj = new Array((rows*26)+1)
+};
+
+/** 
+ * @param {string} cell 
+ * @param {number} value
+ * @return {void}
+ */
+Spreadsheet.prototype.setCell = function(cell, value) {
+    cell = Number((cell.charCodeAt(0)-65) * 26) + Number(cell.charCodeAt(1))
+    obj[cell] = value
+};
+
+/** 
+ * @param {string} cell
+ * @return {void}
+ */
+Spreadsheet.prototype.resetCell = function(cell) {
+    cell = Number((cell.charCodeAt(0)-65) * 26) + Number(cell.charCodeAt(1))
+    obj[cell] = 0
+};
+
+/** 
+ * @param {string} formula
+ * @return {number}
+ */
+Spreadsheet.prototype.getValue = function(formula) {
+    formula = formula.slice(1).split("+")
+    if (formula[0].charCodeAt(0) > 60){
+        let cell = Number((formula[0].charCodeAt(0)-65) * 26) + Number(formula[0].charCodeAt(1))
+        formula[0] = obj[cell] || 0
+        }
+    if (formula[1].charCodeAt(0) > 60){
+        let cell2 = Number((formula[1].charCodeAt(0)-65) * 26) + Number(formula[1].charCodeAt(1))
+        formula[1] = obj[cell2] || 0
+        }
+    return Number(formula[0]) + Number(formula[1])
+};
+
+/** 
+ * Your Spreadsheet object will be instantiated and called as such:
+ * var obj = new Spreadsheet(rows)
+ * obj.setCell(cell,value)
+ * obj.resetCell(cell)
+ * var param_3 = obj.getValue(formula)
+ */
+
+/**
+ * @param {number} rows
+ */
+var Spreadsheet = function(rows) {
+    obj = new Array((rows*26)+1)
+    mult = rows
+};
+
+/** 
+ * @param {string} cell 
+ * @param {number} value
+ * @return {void}
+ */
+Spreadsheet.prototype.setCell = function(cell, value) {
+    newCell = Number((cell.charCodeAt(0)-65) * mult) + Number(cell.slice(1))
+    obj[newCell] = value
+};
+
+/** 
+ * @param {string} cell
+ * @return {void}
+ */
+Spreadsheet.prototype.resetCell = function(cell) {
+    newCell = Number((cell.charCodeAt(0)-65) * mult) + Number(cell.slice(1))
+    obj[newCell] = 0
+};
+
+/** 
+ * @param {string} formula
+ * @return {number}
+ */
+Spreadsheet.prototype.getValue = function(formula) {
+    formula = formula.slice(1).split("+")
+    if (formula[0].charCodeAt(0) > 60){
+        let cell = Number((formula[0].charCodeAt(0)-65) * mult) + Number(formula[0].slice(1))
+        formula[0] = obj[cell] || 0
+        }
+    if (formula[1].charCodeAt(0) > 60){
+        let cell2 = Number((formula[1].charCodeAt(0)-65) * mult) + Number(formula[1].slice(1))
+        formula[1] = obj[cell2] || 0
+        }
+    return Number(formula[0]) + Number(formula[1])
+};
+
+/** 
+ * Your Spreadsheet object will be instantiated and called as such:
+ * var obj = new Spreadsheet(rows)
+ * obj.setCell(cell,value)
+ * obj.resetCell(cell)
+ * var param_3 = obj.getValue(formula)
+ */
