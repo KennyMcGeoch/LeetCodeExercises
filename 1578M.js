dc4ac7b1815d995c21dcc982f94e8ba8d4c65d9e
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2025-11-03 23:40:33 +0000

    1578M

--- 1578M.js
+++ 1578M.js
@@ -52,3 +52,58 @@ var minCost = function(colors, neededTime) {
     return total
     
 };
+
+/**
+ * @param {string} colors
+ * @param {number[]} neededTime
+ * @return {number}
+ * Runtime 6ms Beats 68.89%
+ * Memory 64.72MB Beats 86.67%
+ */
+var minCost = function(colors, neededTime) {
+    
+    let ans = 0
+    let min = neededTime[0]
+
+    for (let i=1; i<colors.length; i++){
+        if (colors[i] === colors[i-1]){
+            ans += Math.min(neededTime[i],min)
+            min = Math.max(neededTime[i],min)
+        }
+        else min = neededTime[i]
+    }
+    
+    return ans
+    
+};
+
+/**
+ * @param {string} colors
+ * @param {number[]} neededTime
+ * @return {number}
+ * Runtime 5ms Beats 84.44%
+ * Memory 64.58MB Beats 91.11%
+ */
+var minCost = function(colors, neededTime) {
+    
+    let ans = 0
+    let min = neededTime[0]
+
+    for (let i=1; i<colors.length; i++){
+        if (colors[i] === colors[i-1]){
+            if (min){
+                ans += Math.min(neededTime[i],min)
+                min = Math.max(neededTime[i],min)
+            }
+            else{
+                ans += Math.min(neededTime[i],neededTime[i-1])
+                min = Math.max(neededTime[i],neededTime[i-1])
+            }
+            
+        }
+        else if (min) min = 0
+    }
+    
+    return ans
+    
+};
