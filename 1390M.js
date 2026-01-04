3d0c6ac34821b4c6dfedd7e4f9780ab1cfbdc8d5
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2026-01-04 19:21:39 +0000

    1390M

---
+++ 1390M.js
@@ -0,0 +1,125 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Runtime 554ms Beats 8.82%
+ * Memory 58.54MB Beats 61.76%
+ */
+
+ let sieve = new Array(50000).fill(1)
+ let primes = []
+ sieve[0] = 0
+ sieve[1] = 0
+ for (let i=2; i<50001; i++){
+    if (sieve[i]){
+        for (let j=i*2; j<50001; j+=i) sieve[j] = 0
+    }
+ }
+ for (let i=0; i<50001; i++){
+    if (sieve[i]) primes.push(i)
+ }
+
+var sumFourDivisors = function(nums) {
+    let ans = 0
+    
+    function calc(target){
+        let left = 0
+        let right = 5132
+
+        while(right > left){
+            if (primes[left] * primes[right] === target) return primes[left] + primes[right] + target + 1
+            else if (primes[left] * primes[right] < target) left++
+            else right--
+        }
+        return 0
+    }
+
+    for (let i=0; i<nums.length; i++){
+        if (primes.includes(Math.cbrt(nums[i]))){
+             ans += (nums[i] + 1 + Math.cbrt(nums[i]) + (Math.cbrt(nums[i]) * Math.cbrt(nums[i])))
+        }
+        else ans += calc(nums[i])
+    }
+
+    return ans
+    
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Runtime 526ms Beats 8.82%
+ * Memory 59.47MB Beats 61.76%
+ */
+
+ let sieve = new Array(50000).fill(1)
+ let primes = []
+ sieve[0] = 0
+ sieve[1] = 0
+ for (let i=2; i<50001; i++){
+    if (sieve[i]){
+        for (let j=i*2; j<50001; j+=i) sieve[j] = 0
+    }
+ }
+ for (let i=0; i<50001; i++){
+    if (sieve[i]) primes.push(i)
+ }
+
+var sumFourDivisors = function(nums) {
+    let ans = 0
+    
+    function calc(target){
+        let left = 0
+        let right = 5132
+        let binleft = 0
+        let cand
+
+        while (primes[right] > target){
+            cand = Math.ceil(((right-binleft)/2) + binleft)
+            if (primes[cand] < target) binleft = cand+1
+            else right = cand -1
+        }
+
+        while(right > left){
+            if (primes[left] * primes[right] === target) return primes[left] + primes[right] + target + 1
+            else if (primes[left] * primes[right] < target) left++
+            else right--
+        }
+        return 0
+    }
+
+    for (let i=0; i<nums.length; i++){
+        if (primes.includes(Math.cbrt(nums[i]))){
+             ans += (nums[i] + 1 + Math.cbrt(nums[i]) + (Math.cbrt(nums[i]) * Math.cbrt(nums[i])))
+        }
+        else ans += calc(nums[i])
+    }
+
+    return ans
+    
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Runtime 44ms Beats 23.53%
+ * Memory 62.91MB Beats 14.71%
+ */
+
+var sumFourDivisors = function(nums) {
+    let ans = 0
+    let arr = []
+    
+    for (let i=0; i<nums.length; i++){
+        arr = []
+        for (j=1; j*j <= nums[i]; j++){
+            if (nums[i] % j === 0){
+                arr.push(j)
+                if (j*j !== nums[i]) arr.push(nums[i]/j)
+            }
+        }
+        if (arr.length === 4) ans += arr.reduce((a,b)=>a+b)
+    }
+
+    return ans
+    
+};
--- README.md
+++ README.md
@@ -456,6 +456,7 @@ Full List of Completed Projects in Numerical Order:
 1380. Lucky Numbers in a Matrix (Easy)
 1381. Design a Stack With Increment Operation (Medium)
 1385. Find the Distance Value Between Two Arrays (Easy)
+1390. Four Divisors (Medium)
 1394. Find Lucky Integer in an Array (Easy)
 1395. Count Number of Teams (Medium)
 1399. Count Largest Group (Easy)
