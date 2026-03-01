23a5e4d28783c004a713ca5f8961a0ac216869e7
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2026-03-01 22:44:38 +0000

    1689M

--- 1689M.js
+++ 1689M.js
@@ -16,3 +16,21 @@ var minPartitions = function(n) {
 
     return Number(ans)
 };
+
+/**
+ * @param {string} n
+ * @return {number}
+ * Runtime 8ms Beats 87.10%
+ * Memory 56.79MB Beats 100.00%
+ */
+var minPartitions = function(n) {
+    let ans = "0"
+    for (let i=0; i<n.length; i++){
+        if (n[i] > ans){
+            ans = n[i]
+            if (ans === "9") return 9
+        }
+    }
+
+    return Number(ans)
+};
