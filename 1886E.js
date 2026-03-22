541571d21b1c14513111f8cf5e6e3393fd28587e
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2026-03-22 21:47:49 +0000

    1886E

--- 1886E.js
+++ 1886E.js
@@ -39,3 +39,53 @@ var findRotation = function(mat, target) {
 
     return false
 };
+
+/**
+ * @param {number[][]} mat
+ * @param {number[][]} target
+ * @return {boolean}
+ * Runtime 0ms Beats 100.00%
+ * Memory 52.92MB Beats 96.49%
+ */
+var findRotation = function(mat, target) {
+    function test(){
+        for (let i=0; i<mat.length; i++){
+            for (let j=0; j<mat.length; j++){
+                if (mat[i][j] !== target[i][j]) return false
+            }
+        }
+        return true
+    }
+    function test90(){
+        for (let i=0; i<mat.length; i++){
+            for (let j=0; j<mat.length; j++){
+                if (mat[i][j] !== target[j][mat.length-i-1]) return false
+            }
+        }
+        return true
+    }
+    function test180(){
+        for (let i=0; i<mat.length; i++){
+            for (let j=0; j<mat.length; j++){
+                if (mat[i][j] !== target[mat.length-i-1][mat.length-j-1]) return false
+            }
+        }
+        return true
+    }
+    function test270(){
+        for (let i=0; i<mat.length; i++){
+            for (let j=0; j<mat.length; j++){
+                if (mat[i][j] !== target[mat.length-j-1][i]) return false
+            }
+        }
+        return true
+    }
+
+    if (test()) return true
+    if (test90()) return true
+    if (test180()) return true
+    if (test270()) return true
+    
+
+    return false
+};
