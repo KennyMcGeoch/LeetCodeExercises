eb080012009c9de81936e723fc1c1990758475a7
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2026-04-19 21:56:37 +0100

    1855M

--- 1855M.js
+++ 1855M.js
@@ -30,3 +30,46 @@
 
     return max
 };
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ * Runtime 4ms Beats 15.15%
+ * Memory 67.57MB Beats 57.58%
+ */
+ var maxDistance = function(nums1, nums2) {
+
+    let max = 0
+    let left = 0
+    let right = 0
+
+    while(left < nums2.length){
+        right = Math.max(right,left)
+        while(nums1[left] <= nums2[right]) right++
+        max = Math.max(max, right- ++left)
+        if (right === nums2.length) return max
+        }
+    return max
+};
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ * Runtime 0ms Beats 100.00%
+ * Memory 67.03MB Beats 78.79%
+ */
+ var maxDistance = function(nums1, nums2) {
+
+    let max = 0
+    let left = 0
+    let right = 0
+
+    while(left < nums1.length && right < nums2.length){
+        if (nums1[left] <= nums2[right]) right++
+        else left++
+        max = Math.max(max, right-left-1)
+        }
+    return max
+};
