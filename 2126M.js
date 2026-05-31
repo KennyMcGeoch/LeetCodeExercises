e3decca2800741701c7a2b4d87051ceb01686914
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2026-06-01 00:54:37 +0100

    2126M

--- 2126M.js
+++ 2126M.js
@@ -17,3 +17,32 @@ var asteroidsDestroyed = function(mass, asteroids) {
 
     return true
 };
+
+/**
+ * @param {number} mass
+ * @param {number[]} asteroids
+ * @return {boolean}
+ * Runtime 9ms Beats 100.00%
+ * Memory 70.91MB Beats 74.07%
+ */
+var asteroidsDestroyed = function(mass, asteroids) {
+    let dest = asteroids.length
+    let hit = true
+
+    while(hit && dest){
+        hit = false
+        for (let i=0; i<asteroids.length; i++){
+            if (asteroids[i]){
+                if (mass >= asteroids[i]){
+                    mass += asteroids[i]
+                    asteroids[i] = 0
+                    dest--
+                    hit = true
+                }
+            }
+        }
+
+    }
+
+    return hit
+};
