04338f886f2abfe38faa75b5c2d2b8e0af94aa82
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2026-06-03 00:24:44 +0100

    3633E

---
+++ 3633E.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} landStartTime
+ * @param {number[]} landDuration
+ * @param {number[]} waterStartTime
+ * @param {number[]} waterDuration
+ * @return {number}
+ * Runtime 1ms Beats 100.00%
+ * Memory 55.90MB Beats 82.05%
+ */
+var earliestFinishTime = function(landStartTime, landDuration, waterStartTime, waterDuration) {
+    let ans = Infinity
+    let landFinish = Infinity
+    let waterFinish = Infinity
+
+    for (let i=0; i<landStartTime.length; i++) landFinish = Math.min(landStartTime[i] + landDuration[i], landFinish)
+    for (let i=0; i<waterStartTime.length; i++) waterFinish = Math.min(waterStartTime[i] + waterDuration[i], waterFinish)
+    for (let i=0; i<landDuration.length; i++) ans = Math.min(ans, Math.max(waterFinish, landStartTime[i])+ landDuration[i])
+    for (let i=0; i<waterDuration.length; i++) ans = Math.min(ans, Math.max(landFinish, waterStartTime[i])+ waterDuration[i])
+
+    return ans
+};
--- README.md
+++ README.md
@@ -959,6 +959,7 @@ Full List of Completed Projects in Numerical Order:
 3606. Coupon Code Validator (Easy)
 3607. Power Grid Maintenance (Medium)
 3623. Count Number of Trapezoids I (Medium)
+3633. Earliest Finish Time for Land and Water Rides I (Easy)
 3637. Trionic Array I (Easy)
 3634. Minimum Removals to Balance Array (Medium)
 3643. Flip Square Submatrix Vertically (Easy)
